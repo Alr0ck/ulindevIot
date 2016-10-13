9ec95dbfd38dbbe3b6cb4a9af6e179e9a02a1b1e
Alr0ck <alfiyanrock@gmail.com>
2016-10-13 11:44:44 +0700

    Create ulindevIot.h

---
+++ ulindevIot.h
@@ -0,0 +1,33 @@
+/***********************************
+Please, don't remove this text!
+
+Created by hcix_c0d3r
+at 13 Oct 2016
+Project name:
+ESP-Arduino Internet Of Things
+visit me : http://ulindev.com
+************************************/
+
+#ifndef ulindevIot_H
+#define ulindevIot_H
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+
+class ulindevIot{
+	public:
+	void begin(int baudrate);
+	bool connectTo(String SSID, String PASS);
+	void sendDataTo(String GET, String IP);
+	void sendDebug(String cmd);
+};
+
+extern ulindevIot ul;
+
+#endif
